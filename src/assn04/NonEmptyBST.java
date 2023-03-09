b265ff85ae30d5be684f4c5ceb20f75e141e0474
Anish Toomu <34148007+av2mu@users.noreply.github.com>
2023-03-08 21:52:54 -0500

    assn04 complete

--- src/assn04/NonEmptyBST.java
+++ src/assn04/NonEmptyBST.java
@@ -21,32 +21,83 @@ public class NonEmptyBST<T extends Comparable<T>> implements BST<T> {
 	// TODO: insert
 	@Override
 	public BST<T> insert(T element) {
-		
+		if(this._element.compareTo(element) < 0 ) {
+			if(this.getRight().isEmpty()){
+				this._right = new NonEmptyBST<T>(element);
+			}
+			else
+				this._right.insert(element);
+		}
+		else{
+			if(this.getLeft().isEmpty()){
+				this._left = new NonEmptyBST<T>(element);
+			}
+			else
+				this._left.insert(element);
+		}
 		return this;
 	}
 
 	// TODO: remove
 	@Override
 	public BST<T> remove(T element) {
+		if (this._element.compareTo(element) == 0){
+			if (this._right.isEmpty()){
+				return this._left;
+			}
+			else if (this._left.isEmpty()){
+				return this._right;
+			}
+			else{
+				BST<T> min = this._right;
+				while (!min.getLeft().isEmpty()){
+					min = min.getLeft();
+				}
+				this._element = min.getElement();
+				this._right = this._right.remove(min.getElement());
+			}
+		}
+		else if (this._element.compareTo(element) < 0){
+			this._right = this._right.remove(element);
+		}
+		else{
+			this._left = this._left.remove(element);
+		}
 		return this;
 	}
 
 	// TODO: printPreOrderTraversal
 	@Override
 	public void printPreOrderTraversal() {
-
+		System.out.print(this._element + " ");
+		this._left.printPreOrderTraversal();
+		this._right.printPreOrderTraversal();
 	}
 
 	// TODO: printPostOrderTraversal
 	@Override
 	public void printPostOrderTraversal() {
-
+		this._left.printPostOrderTraversal();
+		this._right.printPostOrderTraversal();
+		System.out.print(this._element + " ");
 	}
 
 	// TODO: printBreadthFirstTraversal
 	@Override
 	public void printBreadthFirstTraversal() {
+		Queue<BST> queue = new LinkedList<>();
+		queue.add(this);
 
+		while (!queue.isEmpty()){
+			BST<T> current = queue.poll();
+			System.out.print(current.getElement() + " ");
+			if(!current.getLeft().isEmpty()){
+				queue.add(current.getLeft());
+			}
+			if (!current.getRight().isEmpty()){
+				queue.add(current.getRight());
+			}
+		}
 	}
 
 	// GetHeight of A Tree
